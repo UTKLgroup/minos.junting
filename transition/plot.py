d1c17290627c9ea56457c59578fdecba0db17cad
juntingh <junting.huang@gmail.com>
2018-02-08 22:46:00 -0600

    added h_stat_syst in plot_c()

--- transition/plot.py
+++ transition/plot.py
@@ -55,13 +55,13 @@ def plot_g():
 
     tl_1_sigma = TLine(0, 1, x_max, 1)
     tl_1_sigma.SetLineStyle(2)
-    tl_1_sigma.Draw('same')
+    tl_1_sigma.Draw()
     tl_2_sigma = TLine(0, 4, x_max, 4)
     tl_2_sigma.SetLineStyle(2)
-    tl_2_sigma.Draw('same')
+    tl_2_sigma.Draw()
     tl_3_sigma = TLine(0, 9, x_max, 9)
     tl_3_sigma.SetLineStyle(2)
-    tl_3_sigma.Draw('same')
+    tl_3_sigma.Draw()
 
     tlatex = TLatex()
     tlatex.SetTextFont(43)
@@ -87,33 +87,77 @@ def plot_g():
 
 
 def plot_c():
-    # f_stat_syst = TFile('{}/GStatsSyst_mc_allRuns_0.root'.format(DATA_DIR))
+    f_stat_syst = TFile('{}/CMuStatsSyst_mc_allRuns_0.root'.format(DATA_DIR))
     f_stat = TFile('{}/CMuStats_mc_allRuns_0.root'.format(DATA_DIR))
 
-    # h_stat_syst = f_stat_syst.Get('hsurface1')
+    h_stat_syst = f_stat_syst.Get('hsurface1')
     h_stat = f_stat.Get('hsurface1')
 
+    subtract_min(h_stat_syst)
     subtract_min(h_stat)
 
     bin_count = h_stat.GetNbinsX()
-    # h_stat_scale = TH1D('h_stat_scale', 'h_stat_scale', bin_count, 0, 10)
     print('bin_count = {}'.format(bin_count))
     print('h_stat.GetBinLowEdge(bin_count) + h_stat.GetBinWidth(bin_count) = {}'.format(h_stat.GetBinLowEdge(bin_count) + h_stat.GetBinWidth(bin_count)))
     print('h_stat.GetBinLowEdge(1) = {}'.format(h_stat.GetBinLowEdge(1)))
 
+    h_stat_syst_scale = TH1D('h_stat_syst_scale', 'h_stat_syst_scale', bin_count, -9.9, 10)
     h_stat_scale = TH1D('h_stat_scale', 'h_stat_scale', bin_count, -9.9, 10)
     for i in range(1, bin_count + 1):
+        h_stat_syst_scale.SetBinContent(i, h_stat_syst.GetBinContent(i))
         h_stat_scale.SetBinContent(i, h_stat.GetBinContent(i))
 
     c1 = TCanvas('c1', 'c1', 800, 600)
     set_margin()
+    gStyle.SetOptStat(0)
 
     set_h1_style(h_stat_scale)
+    h_stat_scale.SetLineColor(kRed + 1)
     h_stat_scale.Draw()
     h_stat_scale.GetXaxis().SetRangeUser(-9, 9)
+    h_stat_scale.GetYaxis().SetRangeUser(0, 10)
     h_stat_scale.GetXaxis().SetTitle('(c_{L})_{#mu#mu}^{TT} or -(c_{L})_{#tau#tau}^{TT} (#times 10^{-23})')
     h_stat_scale.GetYaxis().SetTitle('-2 #Deltalog #lambda')
 
+    set_h1_style(h_stat_syst_scale)
+    h_stat_syst_scale.Draw('sames')
+
+    lg1 = TLegend(0.34, 0.57, 0.59, 0.69)
+    set_legend_style(lg1)
+    lg1.SetTextSize(23)
+    lg1.AddEntry(h_stat_scale, 'Statistics Only', 'l')
+    lg1.AddEntry(h_stat_syst_scale, 'With Systematics', 'l')
+    lg1.Draw()
+
+    x_min = -9
+    x_max = 9
+    tl_1_sigma = TLine(x_min, 1, x_max, 1)
+    tl_1_sigma.SetLineStyle(2)
+    tl_1_sigma.Draw()
+    tl_2_sigma = TLine(x_min, 4, x_max, 4)
+    tl_2_sigma.SetLineStyle(2)
+    tl_2_sigma.Draw()
+    tl_3_sigma = TLine(x_min, 9, x_max, 9)
+    tl_3_sigma.SetLineStyle(2)
+    tl_3_sigma.Draw()
+
+    tlatex = TLatex()
+    tlatex.SetTextFont(43)
+    tlatex.SetTextSize(23)
+
+    tlatex.DrawLatex(-8.5, 1.2, '1 #sigma')
+    tlatex.DrawLatex(-8.5, 4.2, '2 #sigma')
+    tlatex.DrawLatex(-8.5, 9.2, '3 #sigma')
+
+    tlatex.DrawLatex(-4.2, 8.2, "10.56 #times10^{20} POT, #nu_{#mu}-mode")
+    tlatex.DrawLatex(-4.2, 7.4, " MINOS Sensitivity")
+
+    preliminary = TLatex()
+    preliminary.SetTextAlign(22)
+    preliminary.SetTextFont(43)
+    preliminary.SetTextSize(28)
+    preliminary.SetTextColor(kRed)
+    preliminary.DrawLatex(0, 9.5, "MINOS PRELIMINARY")
 
     c1.Update()
     c1.SaveAs('{}/plot_c.pdf'.format(FIGURE_DIR))
